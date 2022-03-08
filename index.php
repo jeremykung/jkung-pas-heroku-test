2173cff26c5c34b7add2565d752accf64ce862da
Jeremy <jeremykung@gmail.com>
2022-03-08 16:00:32 +0800

    initial commit

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <?php
-    // isset($_POST['submit']) {
+    isset($_POST['submit']) {
 
-    //     // Connect to DB
-    //     $servername = "us-cdbr-east-05.cleardb.net";
-    //     $username = "be4c22fe1bd451";
-    //     $password = "0128e3d6";
-    //     $database = "heroku_f4c1f1b843cd581"
+        // Connect to DB
+        $servername = "us-cdbr-east-05.cleardb.net";
+        $username = "be4c22fe1bd451";
+        $password = "0128e3d6";
+        $database = "heroku_f4c1f1b843cd581";
 
-    //     try {
-    //         $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    //         // set the PDO error mode to exception
-    //         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //         echo "Connected successfully";
-    //     } catch(PDOException $e) {
-    //         echo "Connection failed: " . $e->getMessage();
-    //     }
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
 
-    //     // Write to DB
-    //     $user = $_POST['username'];
-    //     $age = $_POST['age'];
-    //     $sql = "INSERT INTO `user` (username, age) VALUES ('$user', '$age')";
-    //     $conn->exec($sql);
-    //     echo "successfully inserted row";
+        // Write to DB
+        $user = $_POST['username'];
+        $age = $_POST['age'];
+        $sql = "INSERT INTO `user` (username, age) VALUES ('$user', '$age')";
+        $conn->exec($sql);
+        echo "successfully inserted row";
 
-    // }
+    }
 ?>
 
 <h1>Heroku Deploy Test</h1>
