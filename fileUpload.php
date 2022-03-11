af259062c3223da70aa9ab77d1f7881c578787b6
Jeremy <jeremykung@gmail.com>
2022-03-11 14:24:42 +0800

    updated uploads for error checking

--- fileUpload.php
+++ fileUpload.php
@@ -1,18 +1,23 @@
 <?php
 
-var_dump($_FILES);
+if (isset($_POST["upload"])) {
 
-echo $_FILES['myUpload']['name'];
+    var_dump($_FILES);
+    echo $_FILES['myUpload']['name'];
+    
+    $destinationFolder = "./images/";
+    $filepath = $destinationFolder.$_FILES['myUpload']['name'];
+    if (move_uploaded_file($_FILES['myUpload']['tmp_name'], $filepath)) {
+        echo "<br>Upload Successful<br>";
+    } else {
+        echo "<br>move_upload_file failed";
+    };
+}
 
-$destinationFolder = "./images/";
-
-$filepath = $destinationFolder.$_FILES['myUpload']['name'];
-
-move_uploaded_file($_FILES['myUpload']['tmp_name'], $filepath);
 
 ?>
 
-<form action="fileUpload.php" method="post" enctype="multipart/form-data">
+<form action="" method="post" enctype="multipart/form-data">
     <input type="file" name="myUpload">
-    <input type="submit">
+    <input type="submit" value="Upload" name="upload">
 </form>
